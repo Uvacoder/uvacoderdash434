256bf54ce65d0d3023635f2f283754d4aa792011
Pablo Penaloza <polpenaloza@gmail.com>
2022-10-31 12:45:46 +0100

    fix(config): nextjs + fleek (#29)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: true,
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+    }
+  },
 }
 
 module.exports = nextConfig
