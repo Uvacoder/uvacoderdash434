fd537acbb62360cffe79c978ab1b3c3f155764eb
Pablo Penaloza <polpenaloza@gmail.com>
2022-11-03 14:51:57 +0100

    fix(static): routing (#34)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: true,
+  devIndicators: {
+    buildActivityPosition: 'bottom-right',
+  },
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      '/': { page: '/' },
+      '/binary-clock': { page: '/binary-clock' },
+      '/details': { page: '/details' },
+      '/dashboard': { page: '/dashboard' },
+      ...defaultPathMap,
+    }
+  },
 }
 
 module.exports = nextConfig
